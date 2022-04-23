5688010852b4dd7e184a02aaf30dc62543e3ea47
lannahirave <nikitinrus404@gmail.com>
2022-04-23 21:36:06 +0300

    Implemented charts api; some precontrollers for search engine;

---
+++ SpotifyWebApplication/Controllers/ChartsController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SpotifyWebApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChartsController : ControllerBase
+    {
+        private readonly spotifyContext _context;
+        public ChartsController(spotifyContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("JsonDataArtistAlbums")]
+        public JsonResult JsonDataArtistAlbums()
+        {
+            var artists = _context.Artists.ToList();
+            List<object> artistAlbum = new List<object>();
+            artistAlbum.Add(new[] { "Артист", "Кількість альбомів"});
+            foreach (var artist in artists)
+            {
+                artistAlbum.Add(new object[] {artist.Name, _context.Albums.Count(c => c.ArtistId == artist.Id)});
+            }
+            return new JsonResult(artistAlbum);
+        }
+        [HttpGet("JsonDataPublishersAlbums")]
+        public JsonResult JsonDataPublishersAlbums()
+        {
+            var publishers = _context.Publishers.ToList();
+            List<object> publisherAlbum = new List<object>();
+            publisherAlbum.Add(new[] { "Релізер", "Кількість альбомів"});
+            foreach (var publisher in publishers)
+            {
+                publisherAlbum.Add(new object[] {publisher.Name, _context.Albums
+                    .Count(c => c.PublisherId == publisher.Id)});
+            }
+            return new JsonResult(publisherAlbum);
+        }
+    }
+
+}
---
+++ SpotifyWebApplication/Controllers/SearchController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SpotifyWebApplication.Models;
+
+namespace SpotifyWebApplication.Controllers
+{
+    public class SearchController : Controller
+    {
+        private readonly spotifyContext _context;
+
+        public SearchController(spotifyContext context)
+        {
+            _context = context;
+        }
+
+        // GET
+        public async Task<IActionResult> Index(string searchValue)
+        {
+            if (searchValue is null || searchValue.Length == 0) return RedirectToAction("Index", "Home");
+            var songs = await _context.Songs.Where(a => a.Name.Contains(searchValue)).Include(d => d.Album).ToListAsync();
+            var albums = await _context.Albums.Where(a => a.Name.Contains(searchValue)).Include(d => d.Publisher).ToListAsync();
+            var artists = await _context.Artists.Where(a => a.Name.Contains(searchValue)).ToListAsync();
+            return View();
+        }
+    }
+}
